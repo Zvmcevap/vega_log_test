8e99d5d050d58a22ce8cc2e68fea8511939046b2
Zvmcevap <zupanc.beno@gmail.com>
2025-11-16 20:19:42 +0100

    increase sleep time

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     loops = 6000
-    sleep_ms = 1
+    sleep_ms = 100
     test_module.run(rank, loops, sleep_ms, async_logger)
     if rank == 0:
         check_logs('logs.json')
